0df387f2fe41fc81366157c667d68392ce87835c
Eitaro Fukamachi <e.arrows@gmail.com>
2015-03-11 17:09:45 +0900

    Change the tarball URL to the specific commit.

--- brew/libbcrypt.rb
+++ brew/libbcrypt.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Libbcrypt < Formula
   homepage "https://github.com/Rudolph-Miller/openwall-bcrypt"
-  url "https://github.com/Rudolph-Miller/openwall-bcrypt/archive/master.tar.gz"
+  url "https://github.com/Rudolph-Miller/openwall-bcrypt/archive/53444382b1f03f41f8757493f26f6eae66320d02.tar.gz"
   version '1.1'
-  sha1 "1f2c1b0e64c54af4d4f0da86ff89212b9bd53d70"
+  sha1 "6ad0f93367e7036e4c4c876d44c39cc00bbb584f"
   def install
     system "make", "library"
     lib.install "libbcrypt.#{version}.dylib"
